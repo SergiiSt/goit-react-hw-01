b65aaaca4d5adf71a9e1e658781a30478278c0d3
SergiiSt <serjerl1991@gmail.com>
2024-06-16 22:46:03 +0200

    add clsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,15 @@
+import clsx from "clsx"
 import css from "./FriendListItem.module.css"
 
 export default function FriendListItem({ friend: { avatar, name, isOnline } }) {
+    
+    const statusClases = clsx(css.paragraph, isOnline ? css.online : css.offline)    
+
     return (
        <div className={css.cardWrap}>
   <img src={avatar} alt="Avatar" width="48" />
   <p className={css.paragraph}>{name}</p>
-  <p className={css.paragraph}>{isOnline? "Online" : "Offline"}</p>
+  <p className={statusClases}>{isOnline? "Online" : "Offline"}</p>
 </div>
 
     )
